b1061d16b261be223314894a29303070b6eb4fc8
ruhtraron <rpulliam@gmail.com>
2016-06-04 16:00:45 -0400

    extracted urls

--- pacersbikeshare.py
+++ pacersbikeshare.py
@@ -1,6 +1,13 @@
 
 import requests
+import json
 
 url = 'https://gbfs.bcycle.com/bcycle_pacersbikeshare/gbfs.json'
 response = requests.get(url)
-print response.json()
+gbfs = response.json()
+print json.dumps(gbfs, indent=4, separators=(',', ': '))
+print gbfs['ttl']
+for u in gbfs['data']['en']['feeds']:
+	#print u['url']
+	response = requests.get(u['url']).json()
+	print response
